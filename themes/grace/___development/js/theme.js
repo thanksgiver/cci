133e8f11115e2bb6d9ab1eccfd8c1e2e0ee639c8
Eugene Yu <coolbee.china@gmail.com>
2018-07-26 14:24:56 -0700

    update @ 2018-07-26

--- themes/grace/___development/js/theme.js
+++ themes/grace/___development/js/theme.js
@@ -1,22 +1,26 @@
 (function($) {
 
-  $("#accordian a").click(function() {
-    var link = $(this);
-    var closest_ul = link.closest("ul");
-    var parallel_active_links = closest_ul.find(".active")
-    var closest_li = link.closest("li");
-    var link_status = closest_li.hasClass("active");
-    var count = 0;
 
-    closest_ul.find("ul").slideUp(function() {
-      if (++count == closest_ul.find("ul").length)
-        parallel_active_links.removeClass("active");
-    });
+  // $("#accordian a").click(function(e) {
+  //   // e.preventDefault();
+  //   var link = $(this);
+  //   var closest_ul = link.closest("ul");
+  //   var parallel_active_links = closest_ul.find(".active")
+  //   var closest_li = link.closest("li");
+  //   var link_status = closest_li.hasClass("active");
+  //   var count = 0;
+  //
+  //   closest_ul.find("ul").slideUp(function() {
+  //     if (++count == closest_ul.find("ul").length)
+  //       parallel_active_links.removeClass("active");
+  //   });
+  //
+  //   if (!link_status) {
+  //     closest_li.children("ul").slideDown();
+  //     closest_li.addClass("active");
+  //   }
+  // })
 
-    if (!link_status) {
-      closest_li.children("ul").slideDown();
-      closest_li.addClass("active");
-    }
-  })
+
 
 })(jQuery);
